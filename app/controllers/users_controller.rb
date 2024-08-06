37b0c86ea4d7da5c90ad009e7f4b11696b747e85
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-08-06 05:56:30 +0000

    [add]users controller記入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,41 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only[:edit, :update]
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to posts_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :name)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
