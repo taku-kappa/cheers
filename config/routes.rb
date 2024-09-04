77fb92dfebf6d80b915354eddb182f66818f2082
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-09-04 08:57:51 +0000

    [add]adminのユーザー一覧表示・ログアウト機能追加（次回はユーザー削除機能を追加）

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+  end
+
   devise_for :users
 
   root to: "homes#top"
